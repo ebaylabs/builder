0e4035a60747fc6c2ebf2514d4aeed71c67a87ad
Prabhakhar Kaliyamurthy <pkaliyamurthy@ebay.com>
2013-11-22 11:31:16 -0800

    Refactor

--- builder/build.py
+++ builder/build.py
@@ -9,70 +9,29 @@ import subprocess
 import os
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('buildspec', help="a yaml file, containing the build specification. For a sample build specification, refer https://github.scm.corp.ebay.com/gist/pkaliyamurthy/1735")
-parser.add_argument('-t', '--format', dest='format', default='tar', 
-        help="package type, ex., 'tar'. expect support for building 'deb' packages. Default is tar")
-parser.add_argument('-d', '--debug', dest='debug', action='store_true', help="run the build in debug mode. enables verbose output.")
-parser.add_argument('-c', '--clean', dest='clean', action='store_true', help="option to clean all the local github repos/venvs that might have been created for this package during previous builds.")
-args = parser.parse_args()
+class Gitter:
+    def pull(uri, repo_dir, logfile):
+        git_checkout('master', repo_dir, logfile)
+        cmd = ['git', 'pull', uri]
+        p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
+        p.wait()
 
-def quit(msg):
-    p.err(msg)
-    p.err("Detailed log in %s" % (log))
-    sys.exit(-1)
+    def clone(uri, repo_dir, logfile):
+        cmd = ['git', 'clone', uri]
+        p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
+        p.wait()
 
-def git_pull(uri, repo_dir, logfile):
-    git_checkout('master', repo_dir, logfile)
-    cmd = ['git', 'pull', uri]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
-    p.wait()
+    def checkout(path, repo_dir, logfile):
+        cmd = ['git', 'checkout', '-f', path]
+        p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
+        p.wait()
 
-def git_clone(uri, repo_dir, logfile):
-    cmd = ['git', 'clone', uri]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
-    p.wait()
-
-def git_checkout(path, repo_dir, logfile):
-    cmd = ['git', 'checkout', '-f', path]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=repo_dir)
-    p.wait()
-
-def build(cwd, logfile):
-    cmd = ['python', 'setup.py', 'build', 'sdist']
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
-    p.wait()
-
-'''
-  creates virtual env.
-  'cwd' is the directory from which the script passed is search for.
-  'venv', the virtualenv to create
-'''
-def create_venv(venv, logfile, cwd):
-    cmd = [u'./scripts/basher', 'CREATE_VENV', venv]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
-    p.wait()
-
-def install(venv, package, logfile, cwd):
-    cmd = [u'./scripts/basher', 'INSTALL', venv, package, version]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
-    p.wait()
-
-def package(path, fpmCmd, debUrl, logfile, cwd):
-    cmd = [u'./scripts/basher', path, fpmCmd, debUrl]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
-    p.wait()
-
-def tar(venv, package, version, logfile, cwd):
-    cmd = [u'./scripts/basher', 'TAR', venv, package, version]
-    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
-    p.wait()
 class Printer:
     PINK = '\033[95m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
-    FAIL = '\033[91m'
+    RED = '\033[91m'
     GREY = '\033[90m'
     ENDC = '\033[0m'
 
@@ -88,7 +47,7 @@ class Printer:
         return self.pre() + msg + "\n"
 
     def err(self, msg):
-        print self.pre() + self.FAIL + msg + self.ENDC
+        print self.pre() + self.RED + msg + self.ENDC
         return self.pre() + msg + "\n"
 
     def debug(self, msg):
@@ -98,8 +57,6 @@ class Printer:
     def log(self, msg):
         print self.pre() + msg
         return self.pre() + msg + "\n"
-
-p = Printer()
 
 '''
 Reads the build configuration yaml
@@ -116,122 +73,171 @@ def readConfig(configFile):
                     p.debug('\t' + line)
         return config
 
+'''
+Build the tar file, that is pypi compatible
+'''
+def build(cwd, logfile):
+    cmd = ['python', 'setup.py', 'build', 'sdist']
+    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
+    p.wait()
+
+'''
+  creates virtual env.
+  'cwd' is the directory from which the script passed is search for.
+  'venv', the virtualenv to create
+'''
+def create_venv(venv, logfile, cwd):
+    cmd = [u'./scripts/basher', 'CREATE_VENV', venv]
+    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
+    p.wait()
+
+'''
+Installs the passed 'package' in the given virtualenv
+'''
+def install(venv, package, logfile, cwd):
+    cmd = [u'./scripts/basher', 'INSTALL', venv, package, version]
+    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
+    p.wait()
+
+'''
+Creates a debian package. Refer scripts/basher for details.
+'''
+def package(path, fpmCmd, debUrl, logfile, cwd):
+    cmd = [u'./scripts/basher', path, fpmCmd, debUrl]
+    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
+    p.wait()
+
+'''
+Creates the tar file. Refer scripts/basher for details.
+'''
+def tar(venv, package, version, logfile, cwd):
+    cmd = [u'./scripts/basher', 'TAR', venv, package, version]
+    p = Popen(cmd, stderr=subprocess.STDOUT, stdout=logfile, cwd=cwd)
+    p.wait()
 
 
-try:
-    config = readConfig(args.buildspec)
-except IOError, e:
-    p.err("Fatal: config file '%s' is not found." % args.buildspec)
-    p.err("For usage, try 'build.py --help'")
-    if args.debug:
-        raise
-    sys.exit(-1)
-except:
-    p.err("CRITICAL: config file '%s' can not be read or invalid." % args.buildspec)
-    p.err("For usage, try 'build.py --help'") 
-    if args.debug:
-        raise
-    sys.exit(-1)
-
-#
-# capture the config in local variables for ease of use
-#
-maintainer = config.get("maintainer")
-package_name = config.get("name")
-version = str(config.get("version"))
-pip_requires = config.get("pip-requires")
-debian_deps = config.get("debian-dependencies")
-deb_url = config.get("deb-package-url")
-
-p.debug(" ")
-p.info("Building '%s' version '%s'" % (package_name, version))
-
-
-
+def main():
+    p = Printer()
+    git = Gitter()
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument('buildspec', help="a yaml file, containing the build specification. For a sample build specification, refer https://github.scm.corp.ebay.com/gist/pkaliyamurthy/1735")
+    parser.add_argument('-t', '--format', dest='format', default='tar', 
+            help="package type, ex., 'tar'. expect support for building 'deb' packages. Default is tar")
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help="run the build in debug mode. enables verbose output.")
+    parser.add_argument('-c', '--clean', dest='clean', action='store_true', help="option to clean all the local github repos/venvs that might have been created for this package during previous builds.")
+    args = parser.parse_args()
 
-basedir = os.getcwd()
-logdir = basedir + "/logs"
-tmpdir = '/tmp/c3build'
-builddir = tmpdir + '/' + package_name + '/' + version  + '/build'
-debdir = tmpdir + '/' + package_name + '/' + version  + '/deb'
-venv = builddir + '/venv'
+    try:
+        config = readConfig(args.buildspec)
+    except IOError, e:
+        p.err("Fatal: config file '%s' is not found." % args.buildspec)
+        p.err("For usage, try 'build.py --help'")
+        if args.debug:
+            raise
+        sys.exit(-1)
+    except:
+        p.err("CRITICAL: config file '%s' can not be read or invalid." % args.buildspec)
+        p.err("For usage, try 'build.py --help'") 
+        if args.debug:
+            raise
+        sys.exit(-1)
 
-if not os.path.isdir(logdir):
-    os.mkdir(logdir)
+    #
+    # capture the config in local variables for ease of use
+    maintainer = config.get("maintainer")
+    package_name = config.get("name")
+    version = str(config.get("version"))
+    pip_requires = config.get("pip-requires")
+    debian_deps = config.get("debian-dependencies")
+    deb_url = config.get("deb-package-url")
 
-logfileName = logdir + "/%s.log" % ( package_name + "_" + version )
-logfile = open(logfileName, 'w+')
+    p.debug(" ")
+    p.info("Building '%s' version '%s'" % (package_name, version))
 
+    basedir = os.getcwd()
+    logdir = basedir + "/logs"
+    tmpdir = '/tmp/c3build'
+    builddir = tmpdir + '/' + package_name + '/' + version  + '/build'
+    debdir = tmpdir + '/' + package_name + '/' + version  + '/deb'
+    venv = builddir + '/venv'
 
+    if not os.path.isdir(logdir):
+        os.mkdir(logdir)
 
-if args.clean:
-    p.log("Cleaning working directory." +  p.FAIL + " Beware 'clean' build takes lot of time!" + p.ENDC)
-    if os.path.isdir(venv):
-        shutil.rmtree(venv)
+    logfileName = logdir + "/%s.log" % ( package_name + "_" + version )
+    logfile = open(logfileName, 'w+')
 
-if os.path.isdir(venv):
-    p.log("Virtualenv exists.")
-    p.debug("  Path: %s" % venv)
-else:
-    os.makedirs(venv)
-    p.log("Creating virtualenv...");
-    create_venv(venv, logfile, basedir)
-    p.log("Virtualenv created: '%s'" % ( venv ))
-
-
-if args.format == 'deb':
-    if os.path.isdir(debdir):
-        if args.clean:
-            shutil.rmtree(debdir)
-    else:
-        os.makedirs(debdir)
-
-''' install '''
-for repo in config.get("git-repos"):
-    ''' remove https '''
-    uri = repo.get('uri')
-    repo_name = uri[uri.rindex("/") + 1:-4]
-    p.log("Installing %s'%s'%s" % (p.GREEN, repo_name, p.ENDC))
-    repo_dir = builddir + '/' + uri[8:uri.rindex("/")]
-    repo_path = repo_dir + '/' + repo_name
-
-    # clean git repository so that we get a fresh clone
     if args.clean:
-        if os.path.isdir(repo_dir):
-            shutil.rmtree(repo_dir)
+        p.log("Cleaning working directory." +  p.RED + " Beware 'clean' build takes lot of time!" + p.ENDC)
+        if os.path.isdir(venv):
+            shutil.rmtree(venv)
 
-    if os.path.isdir(repo_path):
-        p.log("  * Found repo. Pulling latest code.")
-        p.debug("    Repo: %s." % repo_path)
-        git_pull(uri, repo_path, logfile)
+    if os.path.isdir(venv):
+        p.log("Virtualenv exists.")
+        p.debug("  Path: %s" % venv)
     else:
-        p.debug("  * Creating directory %s for cloning" % (repo))
-        os.makedirs(repo_dir)
-        p.log("  * Cloning '%s' under '%s'" % (uri, repo_dir))
-        git_clone(uri, repo_dir, logfile)
-    path = repo.get('path')
-    git_checkout(str(path), repo_path, logfile)
-    p.log("  * Building '%s'" % (repo_name))
-    p.debug("    using dir: '%s'" % (repo_path))
-    build(repo_path, logfile)
-    package_tar = subprocess.check_output(["ls", repo_path + "/dist/"]).rstrip('\n')
-    tarname = repo_path + '/dist/' + package_tar
-    p.log("  * Installing '%s'" % package_tar)
-    p.debug("    using tar: %s" % tarname)
-    install(venv, tarname, logfile, basedir)
-
-''' install all pip requirements '''
-p.log("Installing " + p.GREEN + "pip dependencies" + p.ENDC)
-for pip_dep in pip_requires:
-    p.log("  * " + pip_dep)
-    install(venv, pip_dep, logfile, basedir)
+        os.makedirs(venv)
+        p.log("Creating virtualenv...");
+        create_venv(venv, logfile, basedir)
+        p.log("Virtualenv created: '%s'" % ( venv ))
 
 
-if args.format == 'tar':
-    tar_name = package_name + '-' + version + '.tar.gz'
-    p.log("Creating tar - %s'%s'%s, just for you. Sit tight." % (p.BLUE, tar_name, p.ENDC))
-    tar(venv, package_name, version, logfile, basedir)
+    if args.format == 'deb':
+        if os.path.isdir(debdir):
+            if args.clean:
+                shutil.rmtree(debdir)
+        else:
+            os.makedirs(debdir)
 
-logfile.close()
-p.log("Detailed log in %s" % logfileName)
+    ''' install '''
+    for repo in config.get("git-repos"):
+        ''' remove https '''
+        uri = repo.get('uri')
+        repo_name = uri[uri.rindex("/") + 1:-4]
+        p.log("Installing %s'%s'%s" % (p.GREEN, repo_name, p.ENDC))
+        repo_dir = builddir + '/' + uri[8:uri.rindex("/")]
+        repo_path = repo_dir + '/' + repo_name
+
+        # clean git repository so that we get a fresh clone
+        if args.clean:
+            if os.path.isdir(repo_dir):
+                shutil.rmtree(repo_dir)
+
+        if os.path.isdir(repo_path):
+            p.log("  * Found repo. Pulling latest code.")
+            p.debug("    Repo: %s." % repo_path)
+            git.pull(uri, repo_path, logfile)
+        else:
+            p.debug("  * Creating directory %s for cloning" % (repo))
+            os.makedirs(repo_dir)
+            p.log("  * Cloning '%s' under '%s'" % (uri, repo_dir))
+            git.clone(uri, repo_dir, logfile)
+        path = repo.get('path')
+        git.checkout(str(path), repo_path, logfile)
+        p.log("  * Building '%s'" % (repo_name))
+        p.debug("    using dir: '%s'" % (repo_path))
+        build(repo_path, logfile)
+        package_tar = subprocess.check_output(["ls", repo_path + "/dist/"]).rstrip('\n')
+        tarname = repo_path + '/dist/' + package_tar
+        p.log("  * Installing '%s'" % package_tar)
+        p.debug("    using tar: %s" % tarname)
+        install(venv, tarname, logfile, basedir)
+
+    ''' install all pip requirements '''
+    p.log("Installing " + p.GREEN + "pip dependencies" + p.ENDC)
+    for pip_dep in pip_requires:
+        p.log("  * " + pip_dep)
+        install(venv, pip_dep, logfile, basedir)
+
+
+    if args.format == 'tar':
+        tar_name = package_name + '-' + version + '.tar.gz'
+        p.log("Creating tar - %s'%s'%s, just for you. Sit tight." % (p.BLUE, tar_name, p.ENDC))
+        tar(venv, package_name, version, logfile, basedir)
+
+    logfile.close()
+    p.log("Detailed log in %s" % logfileName)
+
+if __name__ == '__main__':
+    main()
